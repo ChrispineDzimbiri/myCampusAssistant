dd092d21bb60740a70a7e45f548d5d1ae2a683ea
ChrispineDzimbiri <chrispinedzimbiri@gmail.com>
2025-06-04 22:39:24 +0200

    Created a dynamic Assignments & Exams screen with RecyclerView

---
+++ app/src/main/java/com/example/mycampusassistant/TaskAdapter.java
@@ -0,0 +1,48 @@
+package com.example.mycampusassistant;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+
+    List<Task> taskList;
+
+    public TaskAdapter(List<Task> taskList) {
+        this.taskList = taskList;
+    }
+
+    public static class TaskViewHolder extends RecyclerView.ViewHolder {
+        TextView titleText, dateText;
+
+        public TaskViewHolder(View itemView) {
+            super(itemView);
+            titleText = itemView.findViewById(R.id.txtTitle);
+            dateText = itemView.findViewById(R.id.txtDueDate);
+        }
+    }
+
+    @NonNull
+    @Override
+    public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.task_item, parent, false);
+        return new TaskViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        Task task = taskList.get(position);
+        holder.titleText.setText(task.getTitle());
+        holder.dateText.setText(task.getDueDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return taskList.size();
+    }
+}
