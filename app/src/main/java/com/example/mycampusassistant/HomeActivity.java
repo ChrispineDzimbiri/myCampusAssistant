ea36e49d6362e0c13858ab058be5cf6c089903a1
0scarBright <bright7677lipenga@gmail.com>
2025-06-05 21:24:56 +0200

    homescreen

--- app/src/main/java/com/example/mycampusassistant/HomeActivity.java
+++ app/src/main/java/com/example/mycampusassistant/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.mycampusassistant;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,16 +10,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        findViewById(R.id.btnSchedule).setOnClickListener(v ->
+                startActivity(new Intent(this, ClassScheduleActivity.class)));
+
+        findViewById(R.id.btnAssignments).setOnClickListener(v ->
+          startActivity(new Intent(this, AssignmentsExamsActivity.class)));
+
+        findViewById(R.id.btnMap).setOnClickListener(v ->
+                startActivity(new Intent(this, CampusMapActivity.class)));
+
+        findViewById(R.id.btnProfile).setOnClickListener(v ->
+                startActivity(new Intent(this, ProfileSettingsActivity.class)));
     }
 }
